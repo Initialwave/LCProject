0d4647670be5d96f9aad42c5248298fd951aab1d
Daniel Ratzlaff <Asylumgames39@gmail.com>
2023-03-02 08:23:09 -0600

    new index.html file

--- src/main/java/com/example/demo/Controllers/LandingPageController.java
+++ src/main/java/com/example/demo/Controllers/LandingPageController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 //import javax.validation.Valid;
 
 @Controller
-@RequestMapping("admin")
+@RequestMapping("")
 
 public class LandingPageController {
 
